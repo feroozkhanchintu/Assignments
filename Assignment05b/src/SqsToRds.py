ff1161011f68fa786e83fefce2b63b524232ff20
Vipul Jain <vjain@codenation.co.in>
2016-07-12 03:19:18 +0400

    added py files for assignment05b

---
+++ Assignment05b/src/SqsToRds.py
@@ -0,0 +1,58 @@
+import datetime
+
+import boto3
+import json
+from sqlalchemy import *
+import config
+
+connection_string = "mysql+pymysql://" + config.USERNAME + ":" + config.PASSWORD + "@" + config.HOST + "/" + config.DATABASE
+engine = create_engine(connection_string)
+metadata = MetaData()
+connection = engine.connect()
+product = Table('LOG_DATA', metadata, autoload=True, autoload_with=engine)
+
+class SqsToRds:
+    def readFromSqs(self):
+        # Get the service resource
+        sqs = boto3.resource('sqs')
+
+        # Get the queue. This returns an SQS.Queue instance
+        queue = sqs.get_queue_by_name(QueueName='cnu2016_vjain_assignment05a')
+
+        # You can now access identifiers and attributes
+        print(queue.url)
+
+        # Process messages by printing out body and optional author name
+        for message in queue.receive_messages():
+            # Get the custom author message attribute if it was set
+            print(message.body)
+            sqs_data = json.loads(message.body)
+            timestamp  = sqs_data['timestamp']
+            t = datetime.datetime.fromtimestamp(float(timestamp) / 1000.0)
+            fmt = "%Y-%m-%d %H:%M:%S"
+            timestamp = t.strftime(fmt)
+            url = sqs_data['url']
+            parameters = sqs_data['parameters']
+            print (type(parameters))
+            responseCode = sqs_data['responseCode']
+            ipAddress = sqs_data['ipAddress']
+            completionTime=sqs_data['completionTime']
+            print (parameters.encode('utf-8'))
+            ins = product.insert().values(TIMESTAMP=timestamp, URL=url, PARAMETERS=parameters, RESPONSECODE=responseCode,
+                                          IPADDRESS=ipAddress, EXECUTIONTIME=completionTime)
+            res = connection.execute(ins)
+            message.delete()
+
+str = SqsToRds()
+# t = threading.Timer(2, str.readFromSqs)
+# t.start()
+while True:
+    import time
+    str.readFromSqs()
+    time.sleep(5)
+
+# str.readFromSqs()
+
+
+
+
---
+++ Assignment05b/src/config.py
@@ -0,0 +1,4 @@
+USERNAME='vjain'
+PASSWORD='vjain'
+HOST='aline-cnu-insights-dev.czuocyoc6awe.us-east-1.rds.amazonaws.com'
+DATABASE='cnu2016_vjain'
